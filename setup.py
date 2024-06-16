798379229782ae64c3942263cd50501db09985fe
Luis Fagundes <lhfagundes@gmail.com>
2024-06-16 14:37:30 -0300

    release v0.0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class DistWithFrontend(sdist):
 setup(
     author="Luis Fagundes",
     author_email='lhfagundes@gmail.com',
-    version='0.0.3',
+    version='0.0.4',
     python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
