8571f8f2b06e3589f2c26f6cb39f1e5b74a65afd
Luis Fagundes <lhfagundes@gmail.com>
2025-02-23 15:30:24 -0300

    fix CadQuery node to work in or outside CQ-editor

--- solid_node/node/adapters/cadquery.py
+++ solid_node/node/adapters/cadquery.py
@@ -6,33 +6,21 @@ import cadquery as cq
 from solid2 import import_stl
 from solid_node.node.leaf import LeafNode
 
-
-def running_inside_cq_editor() -> bool:
-    """Detect if we are running inside CQ-Editor"""
-    for frame in inspect.stack():
-        module = inspect.getmodule(frame.frame)
-        if module and module.__name__ == 'cq_editor.__main__':
-            return True
-    return False
-
-
-class TypeDecider(type):
+class CheckCQEditor(type):
     """This metaclass will check if we are in the context of
     CQ-editor, if so, use no base classes, otherwise inherit
     LeafNode."""
     def __new__(mcs, name, bases, namespace):
-        if running_inside_cq_editor():
+        if sys.modules.get('cq_editor.__main__', None):
             bases = tuple()
-        else:
-            bases = (LeafNode,)
+
         return super().__new__(mcs, name, bases, namespace)
 
 
-class CadQueryNode(metaclass=TypeDecider):
+class CadQueryNode(LeafNode, metaclass=CheckCQEditor):
     """
     Represents a 3D object created using the CadQuery tool.
     """
-
     namespace = 'cadquery.cq'
 
     def as_scad(self, rendered):
